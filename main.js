99b0bf57260e754270a6ef4f61a4f3eff212da47
Giovanne Azevedo Marrone <182200226@senacinformatica.br>
2022-07-14 16:42:29 -0300

    remove item feature done

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
 function toggleDarkMode() {
-    var element = document.body;
-  
-    element.classList.toggle("darkmode");
-  }
+  var element = document.body;
 
-
-
-const achievements = [{}]
-
-function newAchievementPrompt() {
-    let newAchievement = prompt("Conte uma pequena grande conquista de hoje! ❤️")   
-
-    const element = `
-    <li>${newAchievement} <button><img src="./assets/trash.svg" alt="Deletar item"></button></li>
-    `
-
-    if(newAchievement === '' || newAchievement == null) {
-        alert("Você precisa adicionar alguma informação para ela entrar na lista")
-    } else {
-        document.getElementById('achievements-list').innerHTML += element;
-
-        achievements.push(newAchievement)
-    }
+  element.classList.toggle("darkmode");
 }
 
-function removeAchievementFromList() {
-    achievements = achievements.splice(0, 1)
+const achievements = [];
+
+function removeAchievementFromList(event) {
+    
+
+    document.getElementById('achievements-list').removeChild(event.target.parentNode.parentNode)
+
+  console.log(event)
+}
+
+// removeAchievementFromList()
+
+function newAchievementPrompt() {
+  let newAchievement = prompt("Conte uma pequena grande conquista de hoje! ❤️");
+
+  const element = `
+    <li class="new-list-item">${newAchievement} <button><img src="./assets/trash.svg" alt="Deletar item" onclick="removeAchievementFromList(event)"></button></li>
+    `;
+
+    
+    if (newAchievement === "" || newAchievement == null) {
+        alert("Você precisa adicionar alguma informação para ela entrar na lista");
+    } else {
+        document.getElementById("achievements-list").innerHTML += element;
+        
+        achievements.push(newAchievement);
+    }
+    console.log(achievements);
 }
